a46cfcb8074ecc80fa8111c5d8e14167e9ac2640
Haneef Ahmed <haneefahmed@Haneefs-Air.T-mobile.com>
2020-09-03 13:14:05 -0700

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Haneef Ahmed on 8/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
